fee4c8d16fe217e5deb41d4ffb8f413af683db77
Josua Burkard <josua.burkard@sdworx.com>
2024-01-20 08:33:52 +0100

    _Registry.ps1

---
+++ _Registry.ps1
@@ -0,0 +1,58 @@
+﻿function Test-RegistryKey {
+    [OutputType('bool')]
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Key
+    )
+
+    $ErrorActionPreference = 'Stop'
+
+    if (Get-Item -Path $Key -ErrorAction Ignore) {
+        $true
+    }
+}
+
+function Test-RegistryValue {
+    [OutputType('bool')]
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Key,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Value
+    )
+
+    $ErrorActionPreference = 'Stop'
+
+    if (Get-ItemProperty -Path $Key -Name $Value -ErrorAction Ignore) {
+        $true
+    }
+}
+
+function Test-RegistryValueNotNull {
+    [OutputType('bool')]
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Key,
+
+        [Parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$Value
+    )
+
+    $ErrorActionPreference = 'Stop'
+
+    if (($regVal = Get-ItemProperty -Path $Key -Name $Value -ErrorAction Ignore) -and $regVal.($Value)) {
+        $true
+    }
+}
